8be3c80ffeb69be0ea34be86c1610cc952db1153
harish-gurjar-758 <gurjarharish758@gmail.com>
2026-03-06 20:17:07 +0530

    fix the bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    uygftgs
+    <div>
+      jgjhg
+    </div>
   )
 }
-
-export default App
